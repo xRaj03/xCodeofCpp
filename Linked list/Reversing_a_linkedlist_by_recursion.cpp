7f8626323a0ec1e0589fce5b1be03634608ccb04
xRaj03 <rajbff03@gmail.com>
2022-09-30 21:48:39 +0530

    Hello there

---
+++ Linked list/Reversing_a_linkedlist_by_recursion.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+
+using namespace std;
+struct Node {
+    int data;
+    struct Node*next;
+}*first=NULL;
+
+void create(int A[], int n){
+
+    int i;
+    struct Node *t, *last;
+    first=(struct Node *)malloc(sizeof(struct Node));
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+
+    for(i=1; i<n; i++){
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+void display(struct Node *p)
+{
+        while (p)
+        {
+            printf("%d", p->data);
+            p=p->next;
+        }
+        
+
+}
+
+/* void reverse(struct Node *p)
+{
+    
+    struct Node *q, *r;
+    p=first;
+    q=NULL;
+    r=NULL;
+    while(p!=NULL){
+        r=q;
+        q=p;
+        p=p->next;
+        q->next=r;
+    }
+    first=q;
+
+
+} */
+
+void recursiveReverse(struct Node *q, struct Node *p)
+{
+    if(p){
+        recursiveReverse(p, p->next);
+        p->next=q;
+    }
+    else{
+        first=q;
+    }
+} 
+
+
+int main(){
+
+    int A[]={1,2,3,4,5};
+    create (A, 5);
+    /* reverse(first); */
+    recursiveReverse(NULL, first);
+    display(first);
+
+    return 0;
+}
