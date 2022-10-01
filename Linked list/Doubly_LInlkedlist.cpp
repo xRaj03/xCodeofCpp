3b3b1e7493824ad9a710b5057879d652f55aea67
xRaj03 <rajbff03@gmail.com>
2022-10-01 20:52:52 +0530

    Hello there

---
+++ Linked list/Doubly_LInlkedlist.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node{
+    struct Node *prev;
+    int data;
+    struct Node* next;
+
+}*first=NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first=(struct Node*)malloc(sizeof(struct Node));
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=first;
+
+    for(i=1; i<n; i++)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        
+    }
+}
+void Display(struct Node *p)
+{
+    while(p)
+    {
+        printf("%d", p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+int lenght(struct Node *p ){
+    int len=0;
+    while(p)
+    {
+        len++;
+        p=p->next;
+    }
+    return len;
+}
+int main(){
+
+    int A[]={10, 20, 30, 40, 50};
+    create(A, 5);
+    printf("\n Lenght is %d\n", lenght(first));
+    Display(first);
+
+
+    return 0;
+}
