d9a40b0472a11fa71dfeea92b254c6e738f86318
xRaj03 <rajbff03@gmail.com>
2022-10-02 14:41:44 +0530

    Hello there

---
+++ Codechef/Finding_Shoes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        int r = n;
+
+        if (m > n)
+        {
+            m = n;
+        }
+        int count = n - m;
+
+        cout << r + count << endl;
+    }
+    return 0;
+}
