7350bea69a2570a920e6f1e126d54c8699e645a6
xRaj03 <rajbff03@gmail.com>
2022-10-10 22:04:59 +0530

    Hello there

---
+++ Codechef/Hashing/Unorderdset.cpp
@@ -0,0 +1,18 @@
+// Program to use Unorderd set in c++
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    unordered_set<int> s;
+    s.insert(10);
+    s.insert(15);
+    s.insert(30);
+    s.insert(35);
+    for (int x : s)
+        cout << x << " ";
+    return 0;
+}
