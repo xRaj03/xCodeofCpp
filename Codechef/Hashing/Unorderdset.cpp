eebf14f1bfe891032d06865f4446756906dc2f9e
xRaj03 <rajbff03@gmail.com>
2022-10-13 12:02:00 +0530

    Hello there

--- Codechef/Hashing/Unorderdset.cpp
+++ Codechef/Hashing/Unorderdset.cpp
@@ -18,16 +18,3 @@ int main()
     return 0;
 } */
 
-#include<iostream>
-
-using namespace std;
-
-int main(){
-
-    int A[]={1,2,3,4,5};
-    for(auto x:A){
-        cout<<x<<endl;
-    }
-
-    return 0;
-}
