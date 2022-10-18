aed87eaa70f29462e1e7fca0de0c83547c188c96
xRaj03 <rajbff03@gmail.com>
2022-10-18 11:02:52 +0530

    Implementing prefix sum

---
+++ Hashing/prefix_sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // Prototype Code to generate prefix sum  
+    int a[]={1,2,3,4,5};
+    for(int i=0; i<5; i++)
+    {
+        a[i]=a[i]+a[i-1];
+    }
+    for(auto x:a)
+    {
+        cout<<x<<endl;
+    }
+
+    return 0;
+}
