c8dbccea8416a351830af02d4b1b7347bd228bab
xRaj03 <rajbff03@gmail.com>
2022-10-18 16:10:05 +0530

    Optimised solution ti find Subarray with given sum

--- Hashing/Subarray_with_given_sum.cpp
+++ Hashing/Subarray_with_given_sum.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -26,10 +27,34 @@ bool isSum(int a[], int n, int sum)
     return false;
 }
 
+// Optimised Solution to find Subarray with given sum
+
+bool ium(int a[], int n, int sum)
+{
+    unordered_set<int>s;
+    int pre_sum =0;
+    for(int i=0; i<n; i++)
+    {
+        if(pre_sum ==sum)
+        {
+            //cout<<"true";
+            return true;
+        }
+        if(s.find(pre_sum-sum)!=s.end())
+        {
+            //cout<<"true";
+            return true;
+        }
+        s.insert(pre_sum);
+    }
+    //cout<<sum;
+    return sum;
+}
+
 int main(){
 
     int A[]={1,2,3,4,5,6};
-    isSum(A, 6, 7);
+    ium(A, 6, 7);
 
 
     return 0;
