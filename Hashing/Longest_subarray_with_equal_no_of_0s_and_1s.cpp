e26de9711fba001e77e8be3469b8d39d937e26a2
xRaj03 <rajbff03@gmail.com>
2022-10-18 23:24:48 +0530

    the logestSub funtion does not produce the resultant output

--- Hashing/Longest_subarray_with_equal_no_of_0s_and_1s.cpp
+++ Hashing/Longest_subarray_with_equal_no_of_0s_and_1s.cpp
@@ -1,8 +1,20 @@
+/*
+Question 
+Input: arr[] = {1, 0, 1, 1, 1, 0, 0}
+Output: 1 to 6
+(Starting and Ending indexes of output subarray)
+
+Input: arr[] = {1, 1, 1, 1}
+Output: No such subarray
+
+Input: arr[] = {0, 0, 1, 1, 0}
+Output: 0 to 3 Or 1 to 4
+*/
 #include<bits/stdc++.h>
 
 using namespace std;
 
-int longestSub(bool a[], int n)
+int longestSub(bool *a, int n)
 {
     int res=0;
     for(int i=0; i<n; i++)
@@ -15,7 +27,7 @@ int longestSub(bool a[], int n)
             else
                 c1++;
             if(c0==c1)
-                res=max(res, j-i+1);
+                res=max(res, j-i+1);// This argument does not fully satisfies the given logic of the question
         }   
     }
     cout<<res;
@@ -25,7 +37,7 @@ int longestSub(bool a[], int n)
 int main(){
 
     bool A[]={1,0,1,1,1,0,0,0};
-    longestSub(A, 8);
+    cout<<longestSub(A, 8);
 
     return 0;
 }
