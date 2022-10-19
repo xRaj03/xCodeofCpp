d356ee6e8a8194fb93d54e90cd00fceda6cfd7da
xRaj03 <rajbff03@gmail.com>
2022-10-19 12:11:57 +0530

    Hello there

--- Hashing/prefix_sum.cpp
+++ Hashing/prefix_sum.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 
 using namespace std;
 
@@ -13,6 +13,18 @@ int main(){
     {
         cout<<x<<endl;
     }
+
+    return 0;
+} */
+#include<iostream>
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    
 
     return 0;
 }
