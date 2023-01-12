01328dbf886f241f361b51d2f3749f0995a33252
xRaj03 <rajbff03@gmail.com>
2023-01-12 16:00:02 +0530

    Hello there bro !

---
+++ OOPs/Class/pointer_to_object.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class rectangle
+{
+    public:
+    int length; int breadth;
+
+    int area(){ return length*breadth; }
+    int perimeter(){ return 2*(length+breadth);}
+};
+
+
+int main(){
+
+    rectangle *p;
+    p=new rectangle;
+    rectangle *q=new rectangle();
+
+    p->length=10; p->breadth=6;    
+    q->length=10; q->breadth=9;    
+
+    cout<<p->area()<<endl;
+    cout<<"-------"<<endl;
+    cout<<q->area()<<endl;
+
+    return 0;
+}
