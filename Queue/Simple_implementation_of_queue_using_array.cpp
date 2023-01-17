9f851cdb63409fd31a830351fdfd522551d722f1
xRaj03 <rajbff03@gmail.com>
2023-01-17 19:54:59 +0530

    Hello there

---
+++ Queue/Simple_implementation_of_queue_using_array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Queue {
+    int cap, rear;
+    int *queue;
+
+    Queue(int c)
+    {
+        cap=c;
+        rear=0;
+        queue = new int[cap];
+    }
+
+};
+
+int main(){
+
+    
+
+    return 0;
+}
