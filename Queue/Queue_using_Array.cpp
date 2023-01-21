21ece1e79e654928c475d7c9ae4f991b5e92f2fd
xRaj03 <rajbff03@gmail.com>
2023-01-21 22:51:03 +0530

    Hello there

--- Queue/Queue_using_Array.cpp
+++ Queue/Queue_using_Array.cpp
@@ -10,6 +10,50 @@ struct Queue
     int *Q;
 };
 
+/* Condition for the queue is empty
+    Empty:
+    If(front==rear)
+
+    full:
+    if(rear==size-1)
+
+    
+*/
+void enqueue(Queue*q, int x)
+{
+    // Condition to check if the queue is full or not
+
+    if(q->rear==q->size-1)
+        cout<<"Queue is full"<<endl;
+    else
+        q->rear++;
+        q->Q[q->rear]=x;
+    
+
+}
+
+int dequeue(Queue*q)
+{
+    // Intializing the variable x as -1 so that it does not point anywhere
+    int x=-1;
+
+    //Condition to check that , if the queue is already empty or not , 
+    // bcz if the queue is already empty then we can't delete the element 
+    // and it will throw an error 
+
+    if(q->front==q->rear)
+        cout<<"Queue is empty"<<endl;
+    
+    // Code to dequeue an element form the queue
+    else{
+        q->front++;
+        x=q->Q[q->front];
+    }
+    return x;
+
+}
+
+
 int main(){
 
     struct Queue q;
