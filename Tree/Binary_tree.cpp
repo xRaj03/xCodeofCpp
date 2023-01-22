e1ea9bce4ef5520fbbde9928ad2fa57c10227b28
xRaj03 <rajbff03@gmail.com>
2023-01-22 21:46:17 +0530

    Modified the Tree file

--- Tree/Binary_tree.cpp
+++ Tree/Binary_tree.cpp
@@ -67,6 +67,24 @@ int height(Node *root)
         return max(height(root->left), height(root->right))+1;
 }
 
+void printKDistance(Node *root, int k)
+{
+    if(root == NULL)
+    {
+        return ;
+
+    }
+    if(k == 0)
+    {
+        cout<<(root->key)<<" ";
+
+    }
+    else{
+        printKDistance(root->left, k-1);
+        printKDistance(root->right, k-1);
+    }
+}
+
 int main(){
 
     Node*root = new Node(10);
@@ -75,7 +93,7 @@ int main(){
     root->right->left=new Node(40);
     root->right->right = new Node(50);
 
-    cout<<height(root);
+    cout<<printKDistance(root , 3);
 
     return 0;
 }
