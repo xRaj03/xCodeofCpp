ea8d215a67d763445ceb9ab63dc4833567826a91
xRaj03 <rajbff03@gmail.com>
2023-03-04 19:10:11 +0530

    Hello there

---
+++ Codechef/Instagram.cpp
@@ -0,0 +1,32 @@
+/* #include <iostream>
+using namespace std;
+
+int main() {
+	int t, a, b, z;
+    
+	while(t--)
+	{
+	    cin>>a>>b;
+	    z=a*10;
+	    if(z>b)
+	        cout<<"YES"<<endl;
+	    else
+	        cout<<"NO"<<endl;
+	}
+	return 0;
+} */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	int x=10;
+	int A[10];
+	cout<<sizeof(x)<<endl;
+	cout<<sizeof(A)<<endl;
+
+
+	return 0;
+}
---
+++ Queue/test.c
@@ -0,0 +1,15 @@
+/* #include<stdio.h>
+#define fun(x)(x*x-2*x+9)
+int main(){
+    float i;
+    i=45/fun(2);
+    printf("%.2f",i);
+    return 0;
+} */
+#include <stdio.h>
+int main()
+{
+    float q='x';
+    printf("%.2f", q);
+    return 0;
+}
