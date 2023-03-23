a577206a0d06bce887259003a8574782399e100a
xRaj03 <rajbff03@gmail.com>
2023-03-23 08:59:17 +0530

    Hello there

--- Projects/Snake_game.cpp
+++ Projects/Snake_game.cpp
@@ -1,111 +1,223 @@
+/* Snake Game using C++ 
+developed by TheKittyKat, 
+improved by Nazim Nazari 
+December 2017 */
+
 #include <iostream>
-#include <SDL.h>
+#include <conio.h>
+using namespace std;
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+void run();
+void printMap();
+void initMap();
+void move(int dx, int dy);
+void update();
+void changeDirection(char key);
+void clearScreen();
+void generateFood();
 
-bool running = true;
+char getMapValue(int value);
+
+// Map dimensions
+const int mapwidth = 20;
+const int mapheight = 20;
+
+const int size = mapwidth * mapheight;
+
+// The tile values for the map
+int map[size];
+
+// Snake head details
+int headxpos;
+int headypos;
+int direction;
+
+// Amount of food the snake has (How long the body is)
+int food = 4;
+
+// Determine if game is running
+bool running;
 
 int main()
 {
-    // Initialize SDL
-    SDL_Init(SDL_INIT_VIDEO);
+    run();
+    return 0;
+}
 
-    // Create the game window
-    SDL_Window *window = SDL_CreateWindow("Snake Game",
-                                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
-    // Create the game renderer
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-    // Set up the game variables
-    int snake_x = SCREEN_WIDTH / 2;
-    int snake_y = SCREEN_HEIGHT / 2;
-    int snake_direction = 0;
-
-    int food_x = rand() % SCREEN_WIDTH;
-    int food_y = rand() % SCREEN_HEIGHT;
-
-    // Start the game loop
-    while (running)
-    {
-        // Handle user input
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
-                running = false;
-            }
-            else if (event.type == SDL_KEYDOWN)
-            {
-                if (event.key.keysym.sym == SDLK_UP)
-                {
-                    snake_direction = 0;
-                }
-                else if (event.key.keysym.sym == SDLK_DOWN)
-                {
-                    snake_direction = 1;
-                }
-                else if (event.key.keysym.sym == SDLK_LEFT)
-                {
-                    snake_direction = 2;
-                }
-                else if (event.key.keysym.sym == SDLK_RIGHT)
-                {
-                    snake_direction = 3;
-                }
-            }
+// Main game function
+void run()
+{
+    // Initialize the map
+    initMap();
+    running = true;
+    while (running) {
+        // If a key is pressed
+        if (kbhit()) {
+            // Change to direction determined by key pressed
+            changeDirection(getch());
         }
-
-        // Update the game state
-        if (snake_direction == 0)
-        {
-            snake_y -= 10;
-        }
-        else if (snake_direction == 1)
-        {
-            snake_y += 10;
-        }
-        else if (snake_direction == 2)
-        {
-            snake_x -= 10;
-        }
-        else if (snake_direction == 3)
-        {
-            snake_x += 10;
-        }
-
-        if (snake_x < 0 || snake_x >= SCREEN_WIDTH || snake_y < 0 || snake_y >= SCREEN_HEIGHT)
-        {
-            running = false;
-        }
+        // Update the map
+        update();
 
         // Clear the screen
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
+        clearScreen();
 
-        // Draw the snake and food
-        SDL_Rect snake_rect = {snake_x, snake_y, 10, 10};
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderFillRect(renderer, &snake_rect);
+        // Print the map
+        printMap();
 
-        SDL_Rect food_rect = {food_x, food_y, 10, 10};
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &food_rect);
-
-        // Update the screen
-        SDL_RenderPresent(renderer);
-
-        // Wait for a short time to limit the frame rate
-        SDL_Delay(100);
+        // delay 0.4 seconds
+        _sleep(400);
     }
 
-    // Clean up and exit
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    // Game Text
+    cout << "\t\tGame Over!" << endl << "\t\tYour score is: " << food;
 
-    return 0;
+    // Stop console from closing instantly
+    cin.ignore();
+}
+
+// Changes snake direction from input
+void changeDirection(char key) {
+    /*
+      W
+    A + D
+      S
+      1
+    4 + 2
+      3
+    */
+    switch (key) {
+    case 'w':
+        if (direction != 2) direction = 0;
+        break;
+    case 'd':
+        if (direction != 3) direction = 1;
+        break;
+    case 's':
+        if (direction != 4) direction = 2;
+        break;
+    case 'a':
+        if (direction != 5) direction = 3;
+        break;
+    }
+}
+
+// Moves snake head to new location
+void move(int dx, int dy) {
+    // determine new head position
+    int newx = headxpos + dx;
+    int newy = headypos + dy;
+
+    // Check if there is food at location
+    if (map[newx + newy * mapwidth] == -2) {
+        // Increase food value (body length)
+        food++;
+
+        // Generate new food on map
+        generateFood();
+    }
+
+    // Check location is free
+    else if (map[newx + newy * mapwidth] != 0) {
+        running = false;
+    }
+
+    // Move head to new location
+    headxpos = newx;
+    headypos = newy;
+    map[headxpos + headypos * mapwidth] = food + 1;
+
+}
+
+// Clears screen
+void clearScreen() {
+    // Clear the screen
+    system("cls");
+}
+
+// Generates new food on map
+void generateFood() {
+    int x = 0;
+    int y = 0;
+    do {
+        // Generate random x and y values within the map
+        x = rand() % (mapwidth - 2) + 1;
+        y = rand() % (mapheight - 2) + 1;
+
+        // If location is not free try again
+    } while (map[x + y * mapwidth] != 0);
+
+    // Place new food
+    map[x + y * mapwidth] = -2;
+}
+
+// Updates the map
+void update() {
+    // Move in direction indicated
+    switch (direction) {
+    case 0: move(-1, 0);
+        break;
+    case 1: move(0, 1);
+        break;
+    case 2: move(1, 0);
+        break;
+    case 3: move(0, -1);
+        break;
+    }
+
+    // Reduce snake values on map by 1
+    for (int i = 0; i < size; i++) {
+        if (map[i] > 0) map[i]--;
+    }
+}
+
+// Initializes map
+void initMap()
+{
+    // Places the initual head location in middle of map
+    headxpos = mapwidth / 2;
+    headypos = mapheight / 2;
+    map[headxpos + headypos * mapwidth] = 1;
+
+    // Places top and bottom walls 
+    for (int x = 0; x < mapwidth; ++x) {
+        map[x] = -1;
+        map[x + (mapheight - 1) * mapwidth] = -1;
+    }
+
+    // Places left and right walls
+    for (int y = 0; y < mapheight; y++) {
+        map[0 + y * mapwidth] = -1;
+        map[(mapwidth - 1) + y * mapwidth] = -1;
+    }
+
+    // Generates first food
+    generateFood();
+}
+
+// Prints the map to console
+void printMap()
+{
+    for (int x = 0; x < mapwidth; ++x) {
+        for (int y = 0; y < mapheight; ++y) {
+            // Prints the value at current x,y location
+            cout << getMapValue(map[x + y * mapwidth]);
+        }
+        // Ends the line for next x value
+        cout << endl;
+    }
+}
+
+// Returns graphical character for display from map value
+char getMapValue(int value)
+{
+    // Returns a part of snake body
+    if (value > 0) return 'o';
+
+    switch (value) {
+        // Return wall
+    case -1: return 'X';
+        // Return food
+    case -2: return 'O';
+    }
 }
---
+++ Projects/ex.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int a=100;
+    int b=5;
+    float resA= (a*b)/100;
+    cout<<resA;
+
+    return 0;
+}
